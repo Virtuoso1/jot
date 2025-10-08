94601975ada0bab7c6a96672b3a1f84945c21217
Wayne Kimutai <96531439+Virtuoso1@users.noreply.github.com>
2025-10-08 18:11:44 +0300

    order 2

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -62,7 +62,7 @@ const [loading, setLoading] = useState(false)
     setLoading(true)
     try {
       // 🔑 Hardcoded test order details
-      const order_id = `1`
+      const order_id = `2`
       const amount = 1.0
       const phone = "254703385412" // replace with your test phone
       const email = "waynekimutai20@gmail.com"
